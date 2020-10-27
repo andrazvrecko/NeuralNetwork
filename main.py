ad278f53f2d0dd0db27f9100730964fd0834a896
andrazvrecko <69682837+andrazvrecko@users.noreply.github.com>
2020-10-27 14:39:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -149,47 +149,4 @@ class SGD_Optimizer:
         layer.biases += -self.current_learning_rate * layer.dbiases
 
 
-X, y = spiral_data(samples=100, classes=3)
 
-layer1 = Layer_Dense(2,64)
-activation1 = AReLU()
-layer2 = Layer_Dense(64,3)
-loss_activation = Softmax_CrossEntropy()
-optimizer = SGD_Optimizer(decay=0.001)
-
-#FORWARD
-for epoch in range(10001):
-
-    layer1.forward(X)
-    activation1.forward(layer1.output)
-    layer2.forward(activation1.output)
-    loss = loss_activation.forward(layer2.output, y)
-
-    # Calculate accuracy from output of activation2 and targets
-    # calculate values along first axis
-
-    predictions = np.argmax(loss_activation.output, axis=1)
-    if len(y.shape) == 2:
-        y = np.argmax(y, axis=1)
-    accuracy = np.mean(predictions==y)
-
-    if not epoch % 100:
-        print(f'epoch: {epoch}, ' +
-              f'acc: {accuracy:.3f}, ' +
-              f'loss: {loss:.3f}, ' +
-              f'lr: {optimizer.current_learning_rate}')
-
-    # BACKWARD
-    loss_activation.backward(loss_activation.output, y)
-    layer2.backward(loss_activation.dinputs)
-    activation1.backward(layer2.dinputs)
-    layer1.backward(activation1.dinputs)
-
-    optimizer.pre_update_params()
-    optimizer.update_layer(layer1)
-    optimizer.update_layer(layer2)
-
-
-
-#plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
-#plt.show()
